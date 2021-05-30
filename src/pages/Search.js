b9fd41ae3a4fd9bfaaae7d4b5bc2d9f3fe65f106
Charlie <charles.j.montoya@gmail.com>
2021-05-29 22:11:19 -0400

    Added Google Script Loading

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,55 @@
+import { useState, useEffect } from "react";
+
+import GOOGLE_API_KEY from "../constants/key";
+
 function Search() {
+    const [loaded, setLoaded] = useState(false);
+    const [query, setQuery] = useState("");
+    const autoCompleteRef = useRef(null);
+
+    const handleOnChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handlePlaceSelect = async setQuery => {
+
+        const businessObject = await autoComplete.getPlace();
+
+
+    }
+
+    useEffect(() => {
+        const scriptTag = document.createElement("script");
+        scriptTag.src = GOOGLE_API_KEY;
+        scriptTag.addEventListener("load", () => setLoaded(true));
+        document.body.appendChild(scriptTag);
+    }, []);
+
+    useEffect(() => {
+        if (!loaded) return;
+
+        // Script is Loaded
+        // Perform Google API call here
+        const autoComplete = new window.google.maps.places.Autocomplete(
+            autoCompleteRef.current,
+            { fields: ['place_id', 'name', 'formatted_address', 'formatted_phone_number', ] }
+          );
+          autoComplete.setFields(["address_components", "formatted_address"]);
+          autoComplete.addListener("place_changed", () =>
+            handlePlaceSelect(updateQuery)
+          );
+
+    }, [loaded]);
+
     return (
         <div>
-            <p>Search Page</p>
+            <input
+                ref={autoCompleteRef}
+                onChange={handleOnChange}
+                placeholder="Enter a Business Name"
+                value={query}
+            />
+            <p>Search Page! {GOOGLE_API_KEY}</p>
         </div>
     );
 }
