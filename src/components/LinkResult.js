1c4a4cd20f4b27b1306bad66d57a44dea10e8d02
Charlie <charles.j.montoya@gmail.com>
2021-06-12 08:24:55 -0400

    added props to link-list-component

--- src/components/LinkResult.js
+++ src/components/LinkResult.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+import "../styles/link-list.scss";
+
 function LinkResult(props) {
+    console.log("Props: ", props);
+
     return (
         <div className="container">
             <div className="card-wrapper">
-                <h2>{props.business.businessName}</h2>
-                <p>{props.business.placeId}</p>
+                <div className="left-wrapper">
+                    <h2>{props.business.businessName}</h2>
+                    <p>{props.business.address}</p>
+                    <p> Review Link: {props.business.placeId}</p>
+                    <p> Map Link: {props.business.mapLink}</p>
+                </div>
+                <div className="right-wrapper">
+                    <div className="link-list-image">
+                    <img src={props.business.photoUrl} alt="1" />
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -13,11 +13,17 @@ const links = [
         businessName: "ACME",
         address: "123 Main St., Stamford, CT 06830",
         placeId: "placeId-1",
+        mapLink: "map-link-1",
+        photUrl:
+            "https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sATtYBwIbQNX62gzaDPB8Mvrv4YVsQS2P2O1JkigQVyAbPqdKe3SHDv4c05znH2IS2u1j22JZIBsT2BR6ZQTMxFXAnoOFQ79GLR7boizHOEwyoaorCMa4mfuCgooOvbSxZCTA1NaUYZBUL4XqF6PwLe--_Pd6Z1eEwiJp6Y9zLqxq5MmhBU76&amp;3u350&amp;4u350&amp;5m1&amp;2e1&amp;callback=none&amp;key=AIzaSyDk80PqHyEMq3a1Ow8vNusNPw1yn5vMNoE&amp;token=87838",
     },
     {
         businessName: "Fratellis",
         address: "345 South St., Stamford, CT 06830",
         placeId: "placeId-2",
+        mapLink: "map-link-2",
+        photoUrl:
+            "https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sATtYBwIbQNX62gzaDPB8Mvrv4YVsQS2P2O1JkigQVyAbPqdKe3SHDv4c05znH2IS2u1j22JZIBsT2BR6ZQTMxFXAnoOFQ79GLR7boizHOEwyoaorCMa4mfuCgooOvbSxZCTA1NaUYZBUL4XqF6PwLe--_Pd6Z1eEwiJp6Y9zLqxq5MmhBU76&amp;3u350&amp;4u350&amp;5m1&amp;2e1&amp;callback=none&amp;key=AIzaSyDk80PqHyEMq3a1Ow8vNusNPw1yn5vMNoE&amp;token=87838",
     },
 ];
 
